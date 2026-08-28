c22a2207c69ce8b29b1ad905ab04eca9f766d337
RAJnish37 <rajnishinteams@gmail.com>
2026-08-28 13:13:48 +0530

    Added a missing padding import

--- app/src/main/java/com/rajnish/rakshabandhan/features/auth/presentation/AuthenticatedAppShell.kt
+++ app/src/main/java/com/rajnish/rakshabandhan/features/auth/presentation/AuthenticatedAppShell.kt
@@ -1,5 +1,6 @@
 package com.rajnish.rakshabandhan.features.auth.presentation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
