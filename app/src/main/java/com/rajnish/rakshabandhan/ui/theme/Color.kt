2b08c969ec16b33b15c9fb8156e83353a709e4fd
RAJNISH ANAND <rajnishanand027@gmail.com>
2026-08-28 18:15:37 +0530

    feat(phase-4e): add warm Rakhi visual palette

--- app/src/main/java/com/rajnish/rakshabandhan/ui/theme/Color.kt
+++ app/src/main/java/com/rajnish/rakshabandhan/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.rajnish.rakshabandhan.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val RakhiMaroon = Color(0xFF7A1F3D)
+val RakhiMaroonDark = Color(0xFF5A142C)
+val RakhiRose = Color(0xFFC85C78)
+val RakhiGold = Color(0xFFD9A441)
+val RakhiCream = Color(0xFFFFF8F1)
+val RakhiBlush = Color(0xFFF9E8EC)
+val RakhiInk = Color(0xFF2F2326)
+val RakhiMuted = Color(0xFF77696E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple80 = Color(0xFFE6B7C7)
+val PurpleGrey80 = Color(0xFFD9CDD1)
+val Pink80 = Color(0xFFF3C8D5)
+
+val Purple40 = RakhiMaroon
+val PurpleGrey40 = RakhiMuted
+val Pink40 = RakhiRose
